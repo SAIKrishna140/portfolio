fa16578fa49ecfe3a818a7fa10cc43d83e684b99
SAIKrishna140 <krishrocks692@gmail.com>
2025-01-03 19:03:41 -0500

    created navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='overflow-x-hidden text-stone-300 antialiased'>
+        <div className='fixed inset-0 -z-10'>
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default Navbar
