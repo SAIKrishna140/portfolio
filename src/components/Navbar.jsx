1db3890d3f98a3ad8f157d69a24c1e25779d2bac
SAIKrishna140 <krishrocks692@gmail.com>
2025-01-12 18:10:13 -0500

    added background

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
-
+import logo from '../assets/raviKumarLogo.webp';
 const Navbar = () => {
   return (
-    <div className='overflow-x-hidden text-stone-300 antialiased'>
-        <div className='fixed inset-0 -z-10'>
-
-        </div>
-      
-    </div>
+<nav className="flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <a href="/" aria-label="Home">
+          <img src={logo} />
+        </a>
+      </div>
+    </nav>
   )
 }
 
 export default Navbar
+ 
