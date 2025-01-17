3cbabcb3453bb4c5ac1ca502e1c2e810e35b3f7f
SAIKrishna140 <krishrocks692@gmail.com>
2025-01-16 19:09:06 -0500

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import logo from '../assets/raviKumarLogo.webp';
+import logo from '../assets/saiKrishnaLogo.png';
 const Navbar = () => {
   return (
 <nav className="flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
         <a href="/" aria-label="Home">
-          <img src={logo} />
+          <img src={logo} className="h-3/5 w-auto" />
         </a>
       </div>
     </nav>
