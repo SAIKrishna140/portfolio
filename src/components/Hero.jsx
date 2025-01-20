9e97437b861d30352f4ab531720931ddaa5d27eb
SAIKrishna140 <krishrocks692@gmail.com>
2025-01-19 21:14:53 -0500

    added download resume

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,12 +13,17 @@ const Hero = () => {
             <div className='w-full lg:w-1/2'>
             <div className='flex flex-col items-center lg:items-start mt-10'>
                 <h2 className='pb-2 text-4xl tracking-tighter lg:text-8xl'>Sai Krishna Manickyam</h2>
-                <span className='bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-4xl tracking-tight text-transparent'>
+                <span className='bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-4xl text-transparent tracking-tight'>
                         Full Stack Developer</span>
                 <p className='my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter'>
                   {HERO_CONTENT}
 
                 </p>
+                <a href='/resume.pdf'
+                target='_blank'
+                rel="noopener noreferrer"
+                download
+                className='bg-white rounded-full p-4 text-sm text-stone-800 mb-10'>Download Resume</a>
                 </div>
                 </div>
         </div>
